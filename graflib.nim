9455773be078b16e029df0df8ffaacf62928e750
mashingan <rahmat.d.ruffy@gmail.com>
2022-11-11 01:35:06 +0700

    refactor api to accept only node type

--- graflib.nim
+++ graflib.nim
@@ -32,28 +32,28 @@
 ##     import graflib
 ##
 ##     # we build unweighted and undirected empty graph
-##     var graph = buildGraph[char, int]()
+##     var graph = buildGraph[char]()
 ##     doAssert(graph.vertices.len == 0)
 ##     doAssert(graph.edges.len == 0)
 ##
 ##     # Add edgest to our graph
 ##     graph.addEdges [
-##       ("origin1", "destination1", 0),
-##       ("origin2", "destination2", 0),
-##       ("origin1", "destination2", 0),
+##       ("origin1", "destination1"),
+##       ("origin2", "destination2"),
+##       ("origin1", "destination2"),
 ##       ("origin1", "origin2", 0)
-##     ].mapIt( initEdge(it[0], it[1], it[2]) )
+##     ].mapIt( initEdge(it[0], it[1]) )
 ##     doAssert(graph.edges.len == 4)
 ##     doAssert(graph.vertices.len == 4)
 ##
 ##     # Find paths from origin to destination node
-##     var paths = graph.paths(initVertex("origin1", 0), initVertex("destination2", 0))
+##     var paths = graph.paths("origin1", "destination2")
 ##     doAssert(paths.len == 2)
-##     doAssert(paths[0] == @[initVertex("origin1", 0), initVertex("destination2", 0)])
+##     doAssert(paths[0] == @["origin1", "destination2"]
 ##
 ##     # degree = indegree + outdegree
-##     doAssert(graph.degree(initVertex("origin1", 0)) == 3)
-##     doAssert(graph.degree(initVertex("origin2", 0)) == 2)
+##     doAssert(graph.degree("origin1") == 3)
+##     doAssert(graph.degree("origin2") == 2)
 ##
 ## When walking the connected graph, we use defined `==`
 ## for Vertex comparison. Hence if we use a specialized type
@@ -62,96 +62,73 @@
 import sequtils, tables, deques, hashes
 from strutils import join
 from algorithm import reverse
+from std/strformat import fmt
 import heapqueue
 from sugar import dump
 
 type
-  Graph*[T, R] = object
+  Graph*[T] = object
     ## Graph type that implemented generically using ``T`` type as label
     ## and ``R`` as weight type.
-    directed*: bool               ## If true, every edge has only a direction
-                                  ## for each time its defined.
-    weighted*: bool               ## Whether the graph considered weighted
-    vertices*: seq[Vertex[T, R]]  ## Vertices or nodes
-    edges*: seq[Edge[T, R]]       ## Edge which spanned between node1 and node2
+    directed*: bool         ## If true, every edge has only a direction
+                            ## for each time its defined.
+    vertices*: seq[T]    ## Vertices or nodes
+    edges*: seq[Edge[T]] ## Edge which spanned between node1 and node2
 
-  Vertices*[T, R] = seq[Vertex[T, R]]
-    ## An alias for sequence of nodes.
+  Vertex*[T] = T  ## Alias left-over from previous dev version
 
-  Vertex*[T, R] = object
-    ## Node representation
-    label*: T     ## Label which used to identify node, usuall ``char`` or
-                  ## ``string``
-    weight*: R    ## Weight that will be used for various operations, such
-                  ## determining most cost efficient path
-
-  Edge*[T, R] = object
+  Edge*[T] = object
     ## Representation of two connected nodes with weight cost.
     node1*: T
     node2*: T
-    weight*: R
 
-  GraphRef*[T, R] = ref Graph[T, R]
+  GraphRef*[T] = ref Graph[T]
     ## Reference-type graph.
+
+template label*(v: untyped): untyped = v
 
 template withinTrail (body: typed): untyped =
   when defined(trail):
     body
 
-
-proc `==`*(a, b: Vertex): bool =
-  a.label == b.label
-
-proc `<`*(a, b: Vertex): bool =
-  if a.weight < b.weight: true
-  else: false
-
 proc `==`*(a, b: Edge): bool =
   result = a.node1 == b.node1 and
-           a.node2 == b.node2 and
-           a.weight == b.weight
+           a.node2 == b.node2
 
-proc hash*(v: Vertex): Hash =
-  !$v.label.hash
-
-proc initEdge*[T, R](n1, n2: T, weight: R): Edge[T, R] =
-  Edge[T, R](node1: n1, node2: n2, weight: weight)
-
-proc initVertex*[T, R](label: T, weight: R): Vertex[T, R] =
-  Vertex[T, R](label: label, weight: weight)
+proc initEdge*[T](n1, n2: T): Edge[T] =
+  Edge[T](node1: n1, node2: n2)
 
 proc isDirected* (graph: Graph): bool = graph.directed
-proc isWeighted*(graph: Graph): bool = graph.weighted
 
 proc contains*(graph: Graph, vertice: Vertex): bool =
   vertice in graph.vertices
 
-proc contains*[T,R] (graph: Graph[T,R], edge: Edge[T,R]): bool =
+proc contains*[T] (graph: Graph[T], edge: Edge[T]): bool =
   if graph.isDirected:
     edge in graph.edges
   else:
-    var swappedEdge = Edge[T, R](node1: edge.node2, node2: edge.node1,
+    var swappedEdge = Edge[T](node1: edge.node2, node2: edge.node1,
       weight: edge.weight)
     edge in graph.edges or swappedEdge in graph.edges
 
-proc contains (edge: Edge, vertex: Vertex): bool =
+proc contains*[T](edge: Edge[T], vertex: Vertex[T]): bool =
   if vertex.label == edge.node1 or vertex.label == edge.node2: true
   else: false
 
-proc addVertices*[T,R](graph: var Graph, vertices: varargs[Vertex[T,R]]) =
+proc addVertices*[T](graph: var Graph[T], vertices: varargs[T]) =
   for vertex in items(vertices):
     if vertex notin graph.vertices:
       graph.vertices.add vertex
 
-proc addEdges*[T, R] (graph: var Graph, edges: varargs[Edge[T, R]]) =
+proc addEdges*[T] (graph: var Graph, edges: varargs[Edge[T]]) =
   for edge in items(edges):
     let
       node1 = edge.node1
       node2 = edge.node2
-      v1 = Vertex[T,R](label: node1, weight: edge.weight)
-      v2 = Vertex[T,R](label: node2, weight: edge.weight)
-      swappedEdge = Edge[T,R](node1: node2,
-        node2: node1, weight: edge.weight)
+      v1 = node1
+      v2 = node2
+      swappedEdge = Edge[T](node1: node2,
+        node2: node1)
 
     if v1 notin graph.vertices:
       graph.vertices.add v1
@@ -164,34 +141,33 @@ proc addEdges*[T, R] (graph: var Graph, edges: varargs[Edge[T, R]]) =
         swappedEdge notin graph.edges:
       graph.edges.add edge
 
-proc buildGraph*[T, R](vertices: openArray[Vertex[T, R]] = @[],
-    edges: openArray[Edge[T, R]] = @[],
-    directed: bool = false, weighted: bool = false): Graph[T, R] =
-  result = Graph[T, R](vertices: @vertices, edges: @edges,
-    directed: directed, weighted: weighted)
+proc buildGraph*[T](vertices: openArray[Vertex[T]] = @[],
+    edges: openArray[Edge[T]] = @[],
+    directed: bool = false, weighted: bool = false): Graph[T] =
+  result = Graph[T](vertices: @vertices, edges: @edges,
+    directed: directed)
   
   for edge in items(edges):
     let
-      v1 = Vertex[T, R](label:edge.node1, weight:edge.weight)
-      v2 = Vertex[T, R](label:edge.node2, weight:edge.weight)
+      v1 = edge.node1
+      v2 = edge.node2
     if v1 notin result.vertices:
       result.vertices.add v1
     elif v2 notin result.vertices:
       result.vertices.add v2
 
-proc neighbors*[T, R](graph: Graph, vertex: Vertex[T, R]):
-    seq[Vertex[T, R]] =
-  result = newSeq[Vertex[T, R]]()
+proc neighbors*[T](graph: Graph[T], vertex: T): seq[T] =
+  result = newSeq[T]()
   for edge in graph.edges:
     let
-      v1 = Vertex[T, R](label: edge.node1, weight: edge.weight)
-      v2 = Vertex[T, R](label: edge.node2, weight: edge.weight)
+      v1 = edge.node1
+      v2 = edge.node2
     if vertex.label == edge.node1 and v2 notin result:
       result.add v2
     elif vertex.label == edge.node2 and v1 notin result:
       result.add v1
 
-proc `$`*(graph: Graph): string =
+proc `$`*[T](graph: Graph[T]): string =
   result = if graph.isDirected: "Directed "
            else: "Undirected "
   result &= "Graph with "
@@ -201,49 +177,48 @@ proc `$`*(graph: Graph): string =
   result &= "\nedges: " & $(graph.edges)
 
 proc `$`*(edge: Edge): string =
-  $edge.node1 & ":" & $edge.node2 & "@" & $edge.weight
+  $edge.node1 & ":" & $edge.node2
 
-proc indegree*(graph: Graph, vertex: Vertex): int =
-  graph.edges.filterIt( it.node2 == vertex.label ).len
+proc indegree*[T](graph: Graph[T], vertex: T): int =
+  graph.edges.filterIt( it.node2 == vertex).len
 
-proc outdegree*(graph: Graph, vertex: Vertex): int =
-  graph.edges.filterIt( it.node1 == vertex.label ).len
+proc outdegree*[T](graph: Graph[T], vertex: T): int =
+  graph.edges.filterIt( it.node1 == vertex).len
 
 proc degree*(graph: Graph, vertex: Vertex): int =
   graph.indegree(vertex) + graph.outdegree(vertex)
 
-proc buildDigraph*[T, R](): Graph =
-  buildGraph[T, R](directed = true)
+proc buildDigraph*[T](): Graph =
+  buildGraph[T](directed = true)
 
-proc newGraph*[T, R](): GraphRef[T, R] =
-  GraphRef[T, R](vertices: @[], edges: @[], directed: false)
+proc newGraph*[T](): GraphRef[T] =
+  GraphRef[T](vertices: @[], edges: @[], directed: false)
 
-proc isConnected*[T, R](graph: Graph[T,R]): bool =
+proc isConnected*[T](graph: Graph[T]): bool =
   result = true
   for vertex in graph.vertices:
-    if (graph.edges.all do (edge: Edge[T, R])->bool: vertex notin edge):
-      result = false
-      break
+    for edge in graph.edges:
+      if vertex notin edge: return false
 
-proc swapEdge[T,R](edge:Edge[T,R]): Edge[T,R] =
-  Edge[T,R](node1: edge.node2, node2: edge.node1, weight: edge.weight)
+proc swapEdge[T](edge:Edge[T]): Edge[T] =
+  Edge[T](node1: edge.node2, node2: edge.node1)
 
 
-proc paths*[T,R](graph: Graph[T,R],v1, v2: Vertex[T,R]):
-    seq[seq[Vertex[T,R]]] =
+proc paths*[T](graph: Graph[T],v1, v2: Vertex[T]):
+    seq[seq[Vertex[T]]] =
   if v1 notin graph.vertices or v2 notin graph.vertices:
     return @[]
 
   var
     edges = if graph.isDirected: graph.edges
             else: graph.edges.concat(graph.edges.map swapEdge)
-    tempresult = newSeq[seq[Vertex[T,R]]]()
+    tempresult = newSeq[seq[Vertex[T]]]()
 
   template outFilt (x: untyped): untyped =
-    var buff = newseq[Vertex[T,R]]()
+    var buff = newseq[Vertex[T]]()
     for edge in edges:
       if x.label == edge.node1:
-        buff.add initVertex(edge.node2, edge.weight)
+        buff.add edge.node2
     buff
 
   var outbounds = outFilt v1
@@ -263,7 +238,7 @@ proc paths*[T,R](graph: Graph[T,R],v1, v2: Vertex[T,R]):
     withinTrail: echo "current nextbound: ", nextbound
     if nextbound == @[]:
       withinTrail: echo "no nextbound"
-    var nextstate = newSeq[Vertex[T,R]]()
+    var nextstate = newSeq[Vertex[T]]()
     for next in nextbound:
       withinTrail: echo "to visit next: ", next
       if next in state:
@@ -281,8 +256,8 @@ proc paths*[T,R](graph: Graph[T,R],v1, v2: Vertex[T,R]):
   withinTrail: echo "tempresult: ", tempresult
   result = tempresult.deduplicate
 
-proc shortestPath*[T,R](graph: Graph[T,R], v1, v2: Vertex[T,R]):
-    seq[Vertex[T,R]] =
+proc shortestPath*[T](graph: Graph[T], v1, v2: T):
+    seq[Vertex[T]] =
   if v1 notin graph or v2 notin graph:
     return @[]
   let conn = if graph.isDirected: graph.edges
@@ -290,16 +265,16 @@ proc shortestPath*[T,R](graph: Graph[T,R], v1, v2: Vertex[T,R]):
   withinTrail:
     dump conn
   var
-    visited = newseq[Vertex[T,R]]()
-    parent = newTable[Vertex[T,R], Vertex[T,R]]()
-    neighbor = initDeque[Vertex[T,R]]()
+    visited = newseq[Vertex[T]]()
+    parent = newTable[Vertex[T], Vertex[T]]()
+    neighbor = initDeque[Vertex[T]]()
     connected = false
 
   template nextVisiting(x: untyped): untyped =
-    var next = initHeapQueue[Vertex[T,R]]()
+    var next = initHeapQueue[Vertex[T]]()
     for edge in conn:
       if x.label == edge.node1:
-        let node = initVertex(edge.node2, x.weight + edge.weight)
+        let node = edge.node2
         next.push node
         if node notin parent: parent[node] = x
     next
@@ -327,10 +302,7 @@ proc shortestPath*[T,R](graph: Graph[T,R], v1, v2: Vertex[T,R]):
   result.add curr
   result.reverse
 
-proc shortestPath*[T, R](graph: Graph[T,R], v1, v2: T): seq[Vertex[T,R]] =
-  graph.shortestPath(initVertex(v1, 0), initVertex(v2, 0))
-
-proc adjacencyMatrix*[T, R](graph: Graph[T,R]): seq[seq[R]] =
+proc adjacencyMatrix*[T](graph: Graph[T]): seq[seq[int]] =
   let
     m = graph.vertices.len
     edges = if graph.isDirected: graph.edges
@@ -339,12 +311,9 @@ proc adjacencyMatrix*[T, R](graph: Graph[T,R]): seq[seq[R]] =
 
   for edge in edges:
     let
-      i = graph.vertices.find(Vertex[T,R](label:edge.node1,
-        weight: edge.weight))
-      j = graph.vertices.find(Vertex[T,R](label:edge.node2,
-        weight: edge.weight))
-    result[i][j] = if graph.isWeighted: edge.weight
-                   else: 1
+      i = graph.vertices.find(edge.node1)
+      j = graph.vertices.find(edge.node2)
+    result[i][j] = 1
 
 proc incidenceMatrix*(graph: Graph): seq[seq[int]] =
   let
@@ -364,12 +333,12 @@ proc incidenceMatrix*(graph: Graph): seq[seq[int]] =
                      elif node.label == edge.node2: -1
                      else: 0
 
-proc deleteVertex*[T,R](graph: var Graph[T,R], vertex: Vertex[T,R]):
+proc deleteVertex*[T](graph: var Graph[T], vertex: Vertex[T]):
     bool =
   let pos = graph.vertices.find(vertex)
   if pos >= 0:
     graph.vertices.delete(pos)
-    graph.edges.keepIf(proc(e: Edge[T,R]): bool =
+    graph.edges.keepIf(proc(e: Edge[T]): bool =
       e.node1 != vertex.label and e.node2 != vertex.label
     )
 
@@ -377,7 +346,7 @@ proc deleteVertex*[T,R](graph: var Graph[T,R], vertex: Vertex[T,R]):
   else:
     false
 
-proc deleteEdge*[T, R](graph: var Graph[T,R], edge: Edge[T,R]): bool =
+proc deleteEdge*[T](graph: var Graph[T], edge: Edge[T]): bool =
   let pos = graph.edges.find(edge)
   if pos >= 0:
     graph.edges.delete(pos, pos)
@@ -385,39 +354,31 @@ proc deleteEdge*[T, R](graph: var Graph[T,R], edge: Edge[T,R]): bool =
   else:
     false
 
-type PriorityNode[T, R] = object
-  node: Vertex[T, R]
-  cost: R
-proc `<`*[T, R](p1, p2: PriorityNode[T, R]): bool = p1.cost < p2.cost
+type PriorityNode[T, C] = object
+  node: Vertex[T]
+  cost: C
 
-proc `a*`*[T, R](graph: var Graph[T, R], v1, v2: T): seq[Vertex[T, R]] =
+proc `<`*[T, C](p1, p2: PriorityNode[T, C]): bool = p1.cost < p2.cost
+
+proc `a*`*[T, C](graph: var Graph[T], start, goal: T): seq[Vertex[T]] =
   ## A* search based on its start (v1) label (v2) to end.
   ## Users need to provide accessible `proc cost(v1, v2: T): R` and
-  ## `proc distance(v1, v2: T): R` with T and R are matched with Graph[T, R].
+  ## `proc distance(v1, v2: T): R` with T and R are matched with Graph[T].
   ## In rare case users could also need to provide operator "+" and "<" for T
   ## that returns R viz ```nim proc `+`(cost1, cost2: R): R``` and
   ## ```nim proc `<`(cost1, cost2: R): bool```.
   ## Additionally users could be needed to provide the hash proc for T,
   ## i.e `proc hash(t: T): Hash`. Check the std/hashes on how to do it.
-  when not compiles(cost(v1, v2)):
-    {.error: "`proc cost[T, R](v1, v2: T): R` is not defined".}
-
-  when not compiles(distance(v1, v2)):
-    {.error: "`proc distance[T, R](v1, v2: T): R` is not defined".}
-
-  let
-    start = Vertex[T, R](label: v1)
-    goal = Vertex[T, R](label: v2)
 
   var
-    costSoFar = initTable[Vertex[T, R], R]()
-    visited = initTable[Vertex[T, R], Vertex[T, R]]()
-    visiting = initHeapQueue[PriorityNode[T, R]]()
-    thecost: R
+    costSoFar = initTable[T, C]()
+    visited = initTable[T, T]()
+    visiting = initHeapQueue[PriorityNode[T, C]]()
+    thecost: C
 
   costSoFar[start] = thecost
   visited[start] = start
-  visiting.push(PriorityNode[T, R](node: start, cost: thecost))
+  visiting.push(PriorityNode[T, C](node: start, cost: thecost))
   while visiting.len > 0:
     let nextpriority = visiting.pop
     let node = nextpriority.node
@@ -430,7 +391,7 @@ proc `a*`*[T, R](graph: var Graph[T, R], v1, v2: T): seq[Vertex[T, R]] =
       if nextnode notin costSoFar or thecost < costSoFar[nextnode]:
         costSoFar[nextnode] = thecost
         let priority = thecost + nextnode.label.distance(goal.label)
-        visiting.push(PriorityNode[T, R](node: nextnode, cost: priority))
+        visiting.push(PriorityNode[T, C](node: nextnode, cost: priority))
         visited[nextnode] = node
         withinTrail:
           echo "added to queue with priority: ", priority
@@ -445,7 +406,7 @@ proc `a*`*[T, R](graph: var Graph[T, R], v1, v2: T): seq[Vertex[T, R]] =
   result.reverse
 
 when isMainModule:
-  var graph = buildGraph[char, int]()
+  var graph = buildGraph[char]()
   #[
   graph.vertices = @['a', 'b', 'c', 'd', 'e']
   graph.addEdges(('a', 'b'), ('a', 'e'), ('b', 'c'))
@@ -475,14 +436,15 @@ when isMainModule:
   graph.addEdges(('a', 'b'), ('b', 'c'), ('c', 'd'))
   ]#
   #graph.directed = true
-  graph.addEdges(Edge[char,int](node1:'a', node2:'b', weight:0),
-    Edge[char, int](node1:'b', node2:'c', weight:0),
-    Edge[char, int](node1:'c', node2:'d', weight:0))
+  graph.addEdges(Edge[char](node1:'a', node2:'b'),
+    Edge[char](node1:'b', node2:'c'),
+    Edge[char](node1:'c', node2:'d'))
 
-  proc makeEdge[T](conns: varargs[tuple[n1, n2: T]]): seq[Edge[T, int]] =
-    result = newSeq[Edge[T, int]]()
-    for edge in conns:
-      result.add Edge[T, int](node1: edge.n1, node2: edge.n2, weight: 0)
+  proc makeEdge[T](conns: varargs[tuple[n1, n2: T]]): seq[Edge[T]] =
+    result = newSeq[Edge[T]](conns.len)
+    for i, edge in conns:
+      result[i] = Edge[T](node1: edge.n1, node2: edge.n2)
+
   #[
   echo "degree a: ", graph.degree('a'), ": ", graph.neighbors('a')
   echo "degree b: ", graph.degree('b'), ": ", graph.neighbors('b')
@@ -490,45 +452,26 @@ when isMainModule:
   ]#
   graph.addEdges(makeEdge[char](('d', 'f'), ('b', 'e'), ('e', 'f'),
     ('f', 'g'), ('g', 'b'), ('a', 'e'), ('c', 'e')))
-  echo "degree a: ", graph.degree(Vertex[char, int](label:'a',
-    weight: 0)), ": ",
-    graph.neighbors(Vertex[char, int](label: 'a', weight: 0))
-  echo "degree b: ", graph.degree(Vertex[char, int](label:'b',
-    weight: 0)), ": ",
-    graph.neighbors(Vertex[char, int](label: 'b', weight: 0))
-  echo "degree c: ", graph.degree(Vertex[char, int](label:'c',
-    weight: 0)), ": ",
-    graph.neighbors(Vertex[char, int](label: 'c', weight: 0))
+  echo "graph edges are\n    ", graph.edges.join("\n    ")
+  echo "degree a: ", graph.degree('a'), ": ", graph.neighbors('a')
+  echo "degree b: ", graph.degree('b'), ": ", graph.neighbors('b')
+  echo "degree c: ", graph.degree('c'), ": ", graph.neighbors('c')
   echo if graph.isConnected: "graph is connected"
        else: "graph is disconnected"
-  echo graph.paths(Vertex[char,int](label:'g', weight:0),
-    Vertex[char,int](label:'d', weight:0)).join("\n")
+  echo "all paths:\n\t", graph.paths('g', 'd').join("\n\t")
   #echo graph.paths('a', 'd')
-  echo "shortest path: ", graph.shortestPath(
-    Vertex[char,int](label:'g', weight:0),
-    Vertex[char,int](label:'d', weight:0))
-  echo "shortest path2: ", graph.shortestPath('g', 'd')
+  echo "shortest path: ", graph.shortestPath('g','d')
   echo "adjacency matrix:"
   for adj in graph.adjacencyMatrix():
-    echo adj
+    echo '\t', adj
 
   func cost(v1, v2: char): int = 1
   func distance(v1, v2: char): int = 0
 
-  echo "A*: ", graph.`a*`('g', 'd')
-  let g = Vertex[char,int](label:'g', weight:0)
+  #echo "A*: ", graph.`a*`[char, int]('g', 'd')
+  echo "A*: ", `a*`[char, int](graph, 'g', 'd')
+  let g = 'g'
   if graph.deleteVertex(g):
     echo "Vertex ", g, " is deleted"
-    echo "now graph is ", graph
-
-  #[
-  var gr = buildGraph[int]()
-  gr.directed = true
-  gr.addVertices(0, 1, 2, 3, 4)
-  gr.addEdges((0, 1), (0, 2), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3),
-    (2, 4))
-  echo gr
-  echo "incidence matrix 2:"
-  for adj in gr.incidenceMatrix():
-    echo adj
-  ]#
+    #echo "now graph is ", graph
+    echo fmt"now graph is {graph}"

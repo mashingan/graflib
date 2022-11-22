c1ce074af440bdadf3f99d1c70955d421144afd9
mashingan <rahmat.d.ruffy@gmail.com>
2022-11-22 19:34:12 +0700

    add test paths with added isCycle feature

---
+++ tests/test_paths.nim
@@ -0,0 +1,76 @@
+import std/unittest
+from std/sugar import dump, collect
+from std/strutils import splitLines, split, isUpperAscii
+from std/sequtils import all
+
+import graflib
+
+## This test is also example of looking for available paths in
+## given input maps.
+## This inputs map is from Advent of Code 2021 Day 12.
+## We also illustrate that some node can be visited once more
+## by defining the `isCycle` func.
+
+let inputs = [
+"""
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+""",
+"""
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+""",
+"""
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+""",
+]
+
+let graphs = collect:
+    for input in inputs:
+        var g = buildGraph[string]()
+        for line in input.splitLines:
+            let nodes = line.split("-", maxsplit = 2)
+            if nodes.len >= 2:
+                g.addEdges(Edge[string](node1: nodes[0], node2: nodes[1]))
+        g
+
+func isCycle(s: string): bool = s.all isUpperAscii
+
+test "Graph paths":
+    let
+        expectedPaths = [10, 19, 226]
+        start = "start"
+        `end` = "end"
+    for i, g in graphs:
+        let paths = g.paths(start, `end`)
+        check expectedPaths[i] == paths.len
